e0129e043e87d53d711ac73f480f794f0d2206f2
nanako <o.o.nanako.o.o@gmail.com>
2017-05-25 00:33:20 +0900

    誕生日おめでとー！

--- Test/ViewController.swift
+++ Test/ViewController.swift
@@ -9,9 +9,51 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var MonthLabel: UILabel!
+    @IBOutlet var DayLabel: UILabel!
+    @IBOutlet var HappyLabel: UILabel!
+    
+    var number1 : Int = 0
+    var number2: Int = 0
+    
+    
+    @IBAction func MonthPlus(){
+        
+        number1 = number1 + 1
+        MonthLabel.text = String(number1)
+        
+        if number1 == 10 && number2 == 27{
+            HappyLabel.text = String("お誕生日おめでとー！")
+            }
+        }
 
+    
+    @IBAction func MonthMinus(){
+        
+        number1 = number1 - 1
+        MonthLabel.text = String(number1)
+    }
+
+    @IBAction func DayPlus(){
+        
+        number2 = number2 + 1
+        DayLabel.text = String(number2)
+    }
+    @IBAction func DayMinus(){
+        
+        number2 = number2 - 1
+        DayLabel.text = String(number2)
+        
+        
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
